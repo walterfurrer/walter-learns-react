6c798e727b198aed3ee49a57d3fbcb7054489d12
Walter Furrer <walterfurrer@proton.me>
2025-07-01 20:30:18 -0400

    Adjust header spacing and max site width

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,11 +5,11 @@ const Header = () => {
   return (
     <header className="flex flex-col gap-8 mt-32">
       <div className="min-w-sm lg:w-full">
-        <h1 className="text-5xl font-bold flex flex-col gap-2 mx-auto text-left lg:flex-row lg:flex-wrap lg:w-full">
-          <span className="px-4 py-2 flex w-fit">Walter Learns</span>{" "}
-          <span className="text-zinc-950 bg-js-yellow px-4 py-2 lg:bg-linear-to-r">JavaScript</span>
-          <span className="bg-ts-blue px-4 py-2">TypeScript</span>{" "}
-          <span className="text-zinc-950 bg-react-blue px-4 py-2">React</span>
+        <h1 className="text-5xl font-bold flex flex-col mx-auto gap-4 text-left lg:flex-row lg:flex-wrap lg:w-full">
+          <span className="p-2 flex w-fit">Walter Learns:</span>{" "}
+          <span className="text-zinc-950 bg-js-yellow p-2 lg:bg-linear-to-r">JavaScript</span>
+          <span className="bg-ts-blue p-2">TypeScript</span>{" "}
+          <span className="text-zinc-950 bg-react-blue p-2">React</span>
         </h1>
       </div>
       <div className="flex justify-end">
